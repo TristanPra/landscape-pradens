cb891af9d7150d65864f194c595d8d5bb1698140
TristanPra <tristan.pradens@orange.fr>
2017-01-30 16:06:06 +0100

    paysage tristan
    chateau

---
+++ Assets/BasicAi.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAi : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	public Transform target;
+	public Transform myTransform; void Update () { transform.LookAt(target);
+
+
+			transform.Translate (Vector3.forward * 5 * Time.deltaTime);
+		}
+}
